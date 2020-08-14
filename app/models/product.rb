f86853c030a02d72c0e5a6c541cfbc16d84d20e8
boborabi <65844843+boborabi@users.noreply.github.com>
2020-08-15 03:49:27 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,15 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+  belongs_to :size
+  belongs_to :brand
+  belongs_to_active_hash :prefecture
+  has_many :product_images
+
+  validates :title,            presence: true
+  validates :text,             presence: true
+  validates :condition,        presence: true
+  validates :shipping_charge,  presence: true
+  validates :deliver_leadtime, presence: true
+  validates :price,            presence: true
 end
