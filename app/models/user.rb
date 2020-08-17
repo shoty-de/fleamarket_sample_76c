11421a0d18b9817a3f244ae6c91c0671621cacd4
Taiyo-Oda <noda0303@gmail.com>
2020-08-17 17:05:58 +0900

    [WIP]usersテーブルの作成

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true, uniqueness: true
+  
+  has_one :profile
+  has_many :addresses
+  has_many :creditcards
+  has_many :products
+end
