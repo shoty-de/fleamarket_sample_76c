7557db311f10b5f4027eb63586b3828633fe7cfe
shoty-de <66251668+shoty-de@users.noreply.github.com>
2020-08-17 23:31:07 +0900

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,6 +4,7 @@ class Address < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
   validates :post_family_name,        presence: true
