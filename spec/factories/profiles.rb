a81e2a0e28a251e1596a8d0e3e232f9564fa1115
boborabi <65844843+boborabi@users.noreply.github.com>
2020-08-22 15:49:32 +0900

    Update spec/factories/profiles.rb

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :profile do
-    
+    user
+    family_name      {"てすと"}
+    family_name_kana {"テスト"}
+    first_name       {"太郎"}
+    first_name_kana  {"タロウ"}
+    birthday         {1}
+    birthmonth       {1}
+    birthyear        {1999}
   end
 end
