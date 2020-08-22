50c5d476cf8105b099b16dcc72cf56765b46b06b
boborabi <65844843+boborabi@users.noreply.github.com>
2020-08-23 00:25:53 +0900

    Update profile.rb, add validates
    全角入力、カナ入力に関するバリデーションを追加した。

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,12 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  
-  validates :family_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name, presence: true
-  validates :first_name_kana, presence: true
+
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
   validates :birthmonth, presence: true
   validates :birthyear, presence: true
-  
+
 end
