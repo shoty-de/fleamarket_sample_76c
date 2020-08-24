b2b5fb2358c5acf4de5491a025346d5115cfb54e
boborabi <65844843+boborabi@users.noreply.github.com>
2020-08-24 13:28:03 +0900

    Update profile model & table, about birthday
    生年月日関するカラムを、birthdayカラムひとつに統合した。
    birthdayカラムの型はdate型とした。

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,7 +6,5 @@ class Profile < ApplicationRecord
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
   validates :birthday, presence: true
-  validates :birthmonth, presence: true
-  validates :birthyear, presence: true
 
 end
