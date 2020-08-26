d18e0f0b34898ad2fe541ed081308d3b3b774eba
boborabi <65844843+boborabi@users.noreply.github.com>
2020-08-26 17:39:30 +0900

    Revise some files, remove comment out
    不要なコメントアウトを削除した。

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
     post 'addresses', to: 'users/registrations#create_address'
   end
   root 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
