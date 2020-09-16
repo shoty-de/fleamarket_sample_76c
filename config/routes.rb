5bacef6fc0d3998aeb9090ce99d94ec6cc9e805c
shoty-de <66251668+shoty-de@users.noreply.github.com>
2020-09-16 13:45:00 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
     post 'profiles', to: 'users/registrations#create_profile'
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
+    post 'products/new', to: 'products#create'
+    get 'products/create', to: 'products#show'
   end
   root 'products#index'
-  resources :products, only: [:index, :new, :create, :show] do
+  resources :products, only: [:index, :new, :create, :show, :edit, :destroy] do
     collection do
       get 'get_children_category', defaults: { format: 'json' }
     end
