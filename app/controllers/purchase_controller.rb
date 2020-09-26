d4e0ca2bbf4cbef59ca2e2f742ea8d071d982823
Taiyo-Oda <noda0303@gmail.com>
2020-09-26 09:30:01 +0900

    条件分岐で表示を変えられるようにしました

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -3,14 +3,13 @@ class PurchaseController < ApplicationController
 
   def index
     card = Creditcard.where(user_id: current_user.id).last
-    if card.blank?
-      # redirect_to controller: "creditcards", action: "new"
-    else
+    if card.present?
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
     @product = Product.find(params[:format])
+    @address = Address.find(current_user.id)
   end
 
   def pay
@@ -18,11 +17,13 @@ class PurchaseController < ApplicationController
     @product = Product.find(params[:format])
     Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
-    :amount => @product.price, 
-    :customer => card.customer_id, 
-    :currency => 'jpy'
-  )
-  redirect_to action: 'done' 
+      :amount => @product.price, 
+      :customer => card.customer_id, 
+      :currency => 'jpy'
+    )
+    @product.buyer_id = current_user.id
+    @product.save
+    redirect_to action: 'done' 
   end
 
 end
