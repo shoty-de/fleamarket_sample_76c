8c1cbbcdb3b7c5d2b331f5513f345425a0ddfc80
Sho Tsuboya <sho_825.sa216@docomo.ne.jp>
2020-09-28 21:24:02 +0900

    Adjust Haml,SCSS,JS

--- app/assets/javascripts/top_category.js
+++ app/assets/javascripts/top_category.js
@@ -1,26 +1,27 @@
 $(function(){
 
-   $(".headerLow__search-style").hover(function(){
-     // 元々display:none;がついてるクラスを取得し、display:none;を外す。
-    var category_parent = $(this).children(".headerLow__search-style__category")
+   $(".parentcategory").hover(function(){
+    var category_parent = $(this).find(".testclass")
+        //  元々display:noneでHTMLを非表示にしている為show()で表示↓
         category_parent.show();
-    }, // 親要素のhoverの外れた時の記述↓
+    },  //  親要素のhoverの外れた時の記述↓
      function(){
-      $(this).children(".headerLow__search-style__category").hide();
+        //  表示されたされたhtmlを再度隠す↓
+      $(this).find(".testclass").hide()
     });
 
     function buildHTML(one_child){
       var html =  `
                     <div class= "a">
-                      <a href= "#", class= "block" > ${one_child.name}</a>
+                      <a href= "#", class= "blocks" > ${one_child.name}</a>
                     </div>
                   `
-      $(".headerLow__search-style").append(html);
+      $(".testclass2").append(html);
     }
 
-   $(document).on("mouseenter", ".block", function(){
-    
-    parent_id = $(this).attr("id")
+   //  親カテゴリにマウスが乗った時のイベント発火↓
+   $(document).on("mouseenter", ".headerLow__search-style__category", function(){
+    parent_id = $(this).children().attr("id")
     $.ajax({
       url: "/products/childs",
       type: "GET",
@@ -28,14 +29,18 @@ $(function(){
       dataType: "json"
     })
     .done(function(child){
-        $(".a").remove()
       child.forEach(function(one_child){
-        buildHTML(one_child)
+      buildHTML(one_child)
       })
     })
     .fail(function(){
-      window.alert("ERROR")
+      window.alert("ERROR");
     })
+   });
+
+  //  親カテゴリからマウスが離れた時のイベント発火↓
+   $(document).on("mouseleave", ".headerLow__search-style__category", function(){
+     $(".a").remove();
    })
    
 });
