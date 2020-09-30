fe48f9297a1f98674bc79869b4d8aa00421d370f
boborabi <65844843+boborabi@users.noreply.github.com>
2020-09-30 16:27:14 +0900

    Revert "Revert "Revise product.js & edit.html.haml""
    画像を追加できない不具合を修正した。

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -2,6 +2,15 @@ $(document).on('turbolinks:load', function () {
   $(function () {
     var dataBox = new DataTransfer();
     var file_field = document.querySelector('input[type=file]')
+
+    // 画像にindexを割り振るための配列を用意
+    var fileIndex = [1, 2, 3, 4, 5];
+
+    // 保存済みの画像の表示にて既に使われているindexを除外
+    lastIndex = $('.item-image:last').data('index');
+    fileIndex.splice(0, lastIndex);
+
+    // 画像を挿入する処理
     $('#img-file').change(function () {
       var files = $('input[type="file"]').prop('files')[0];
       $.each(this.files, function (i, file) {
@@ -17,48 +26,52 @@ $(document).on('turbolinks:load', function () {
         }
         fileReader.onloadend = function () {
           var src = fileReader.result
-          var html = `<div class='item-image' data-image="${file.name}">
-                      <div class=' item-image__content'>
-                        <div class='item-image__content--icon'>
-                          <img src=${src} width="114" height="80" >
-                        </div>
-                      </div>
-                      <div class='item-image__operetion'>
-                        <div class='item-image__operetion--delete'>削除</div>
-                      </div>
-                    </div>`
+          const buildFileField = (index) => {
+            const html = `<div class='item-image' data-index="${index}">
+                            <div class=' item-image__content'>
+                              <div class='item-image__content--icon'>
+                                <img src=${src} width="114" height="80" >
+                              </div>
+                            </div>
+                            <div class='item-image__operetion'>
+                              <div class='item-image__operetion--delete'>削除</div>
+                            </div>
+                          </div>`
 
-          $('#image').before(html);
+            return html;
+          }
+
+          $('#image').before(buildFileField(fileIndex[0]));
         };
 
         $('#image').attr('class', `input-area-${num}`)
       });
+      console.log(file_field.files.length + 10);
     });
+    // チェックボックスを非表示にするcss
+    $('.hidden-destroy').hide();
 
-    // チェックボックスを非表示にするcss（現時点では動作確認用に表示させている。完成したら非表示にする事）
-    // $('.hidden-destroy').hide();
-
+    // 画像を削除する処理
     $(document).on("click", '.item-image__operetion--delete', function () {
       var target_image = $(this).parent().parent()
-      var target_name = $(target_image).data('image')
+      var target_index = $(target_image).data('index')
 
-      // ↓追加
-      const hiddenCheck = $(`input[data-image="${target_name}"].hidden-destroy`);
-      // もしチェックボックスが存在すればチェックを入れる
+      const hiddenCheck = $(`input[data-index="${target_index}"].hidden-destroy`);
       if (hiddenCheck) {
         hiddenCheck.prop('checked', true)
+        // console.log(target_index);
       };
-      // ↑追加
-      // 「削除」をクリックするとチェックボックスにチェックが入るが、submitと結び付けられていないためDBから削除が出来ない
+      if (hiddenCheck.prop('checked', true)) {
+        // console.log("チェック");
+        // console.log(hiddenCheck);
+      };
 
-      // file_field.files：挿入された画像ファイルの配列
       if (file_field.files.length == 1) {
         $('input[type=file]').val(null)
         dataBox.clearData();
-        console.log(dataBox)
       } else {
         $.each(file_field.files, function (i, input) {
-          if (input.name == target_name) {
+          if (input.name == target_index) {
             dataBox.items.remove(i)
           }
         })
@@ -68,6 +81,7 @@ $(document).on('turbolinks:load', function () {
       var num = $('.item-image').length
       $('#image').show()
       $('#image').attr('class', `input-area-${num}`)
+      console.log(file_field.files.length + 100);
     })
   });
 });
