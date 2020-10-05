11727019254e8e8b93b462e5a5c63d8273dadd6b
Sho Tsuboya <sho_825.sa216@docomo.ne.jp>
2020-10-05 23:30:22 +0900

    Add new function, adjust

--- app/assets/javascripts/top_category.js
+++ app/assets/javascripts/top_category.js
@@ -1,5 +1,7 @@
 $(function(){
 
+/* カテゴリの文字に乗った際に起こるイベント */
+
    $(".parentcategory").hover(function(){
     var category_parent = $(this).find(".testclass")
         //  元々display:noneでHTMLを非表示にしている為show()で表示↓
@@ -9,17 +11,31 @@ $(function(){
         //  表示されたされたhtmlを再度隠す↓
       $(this).find(".testclass").hide()
     });
+    
+/* 親カテゴリに乗った際に起こるイベント関連 */
 
-    function buildHTML(one_child){
-      var html =  `
-                    <div class= "a">
-                      <a href= "#", class= "blocks" > ${one_child.name}</a>
-                    </div>
-                  `
+    // 小カテゴリを作成する関数
+    function OptionHTML(one_child){
+      var children_category = `<div class= "a">
+                                  <a href= "#", class= "blocks" > ${one_child.name}</a>
+                               </div>
+                              `
+    // 返り値をしっかり定義させる 理由は？
+      return children_category
+    }
+
+    // 親カテゴリに一回に対して一回のみ発火(子カテゴリは元々OptionHTMLにて作成してあるのを一緒に持ってくる)
+    function AppendChildBox(insertHTML){ 
+      var html = ""; // これ何？
+      html =  `
+              <div class="whitebox">
+                ${insertHTML}
+              </div>
+              `
       $(".testclass2").append(html);
     }
 
-   //  親カテゴリにマウスが乗った時のイベント発火↓
+   //  親カテゴリに乗った時の実際のイベント発火
    $(document).on("mouseenter", ".headerLow__search-style__category", function(){
     parent_id = $(this).children().attr("id")
     $.ajax({
@@ -29,19 +45,21 @@ $(function(){
       dataType: "json"
     })
     .done(function(child){
-      $(".a").remove();
+      $(".whitebox").remove();
+      insertHTML = ""  // これ何？
       child.forEach(function(one_child){
-      buildHTML(one_child)
+      insertHTML = insertHTML + OptionHTML(one_child)
       })
+      AppendChildBox(insertHTML);
     })
     .fail(function(){
       window.alert("ERROR");
     })
    });
 
-  //  親カテゴリと小カテゴリをまとめるdiv要素からマウスが離れた時のイベント発火であり、Appendした小カテゴリを消す。↓
+  //  親カテゴリと小カテゴリをまとめるdiv要素からマウスが離れた時のイベント発火であり、AppendChildBox関数でappendした小カテゴリを消す。↓
    $(document).on("mouseleave", ".test", function(){
-    $(".a").remove();
+    $(".whitebox").remove();
   })
    
 });
