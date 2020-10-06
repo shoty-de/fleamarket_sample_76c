f216f0d7cb267ca2e2f4bdb42699c46cf64ed2ce
Sho Tsuboya <sho_825.sa216@docomo.ne.jp>
2020-10-06 16:02:25 +0900

    Changed class name

--- app/assets/javascripts/top_category.js
+++ app/assets/javascripts/top_category.js
@@ -3,20 +3,20 @@ $(function(){
 /* カテゴリの文字に乗った際に起こるイベント */
 
    $(".parentcategory").hover(function(){
-    var category_parent = $(this).find(".testclass")
+    var category_parent = $(this).find(".parents_class")
         //  元々display:noneでHTMLを非表示にしている為show()で表示↓
         category_parent.show();
     },  //  親要素のhoverの外れた時の記述↓
      function(){
         //  表示されたされたhtmlを再度隠す↓
-      $(this).find(".testclass").hide()
+      $(this).find(".parents_class").hide()
     });
     
 /* 親カテゴリに乗った際に起こるイベント関連 */
 
     // 小カテゴリを作成する関数
-    function OptionHTML(one_child){
-      var children_category = `<div class= "a">
+    function ChildrenHTML(one_child){
+      var children_category = `<div class= "Block">
                                   <a href= "#", class= "blocks" > ${one_child.name}</a>
                                </div>
                               `
@@ -25,14 +25,14 @@ $(function(){
     }
 
     // 親カテゴリに一回に対して一回のみ発火(子カテゴリは元々OptionHTMLにて作成してあるのを一緒に持ってくる)
-    function AppendChildBox(insertHTML){ 
+    function MakeChildrenBox(insertHTML){ 
       var html = ""; // これ何？
       html =  `
               <div class="whitebox">
                 ${insertHTML}
               </div>
               `
-      $(".testclass2").append(html);
+      $(".children_class").append(html);
     }
 
    //  親カテゴリに乗った時の実際のイベント発火
@@ -48,17 +48,17 @@ $(function(){
       $(".whitebox").remove();
       insertHTML = ""  // これ何？
       child.forEach(function(one_child){
-      insertHTML = insertHTML + OptionHTML(one_child)
+      insertHTML = insertHTML + ChildrenHTML(one_child) // これ何？
       })
-      AppendChildBox(insertHTML);
+      MakeChildrenBox(insertHTML);
     })
     .fail(function(){
       window.alert("ERROR");
     })
    });
 
-  //  親カテゴリと小カテゴリをまとめるdiv要素からマウスが離れた時のイベント発火であり、AppendChildBox関数でappendした小カテゴリを消す。↓
-   $(document).on("mouseleave", ".test", function(){
+  //  親カテゴリと小カテゴリをまとめるdiv要素からマウスが離れた時のイベント発火であり、MakeChildrenBox関数でappendした小カテゴリを消す。↓
+   $(document).on("mouseleave", ".parents_children_box", function(){
     $(".whitebox").remove();
   })
    
